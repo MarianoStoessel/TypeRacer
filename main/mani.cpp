ed4e0b3c336d7f400370a96bb2d80cf36e00d1e8
Santiago Escobar <santiago.escobar97@gmail.com>
2023-11-09 14:16:07 -0300

    mani ejemplo
    Se creo en el main un ejemplo para verificar que funcione Sprite Leo.
    
    No se toca la clase donde esta la aplicacion para que no haya problemas

--- main/mani.cpp
+++ main/mani.cpp
@@ -10,7 +10,7 @@ int main()
 */
 
 /// MAIN DE PRUEBAS - MECANICA PRINCIPAL DEL JUEGO
-
+/*
 int main()
 {
     Keyword aux("hola mundo");
@@ -27,5 +27,47 @@ int main()
     prueba.compararKeyWord(aux);
 
     system("pause");
+    return 0;
+}
+*/
+
+/// MAIN PRUEBAS - LEO SPRITE
+int main(){
+    //Inicializacion programa
+    sf::RenderWindow window(sf::VideoMode(1500, 900), "LEO PRUEBA");
+    window.setFramerateLimit(60);
+
+    Leo Pj;
+
+    //Game Loop (update del juego
+
+    while(window.isOpen())
+    {
+        //ReadInput - Actualizar los estados de los perisfericos de entrada
+        //Leer la cola de mensajes
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        //CMD - Joy
+        Pj.cmd();
+
+        //Update - Actualiza los estados del juego
+        Pj.update();
+
+        window.clear();
+
+        //Draw
+        window.draw(Pj);
+
+        //Display - Flip
+        window.display();
+    }
+
+    //Liberacion programa
+
     return 0;
 }
